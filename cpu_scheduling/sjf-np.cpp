1613e05172a89d34d56999a2882ba34db07de33b
Kartikey Tewari <kartikeya30@gmail.com>
2020-09-22 23:23:22 +0530

    added code for sjf-premptive

--- cpu_scheduling/sjf-np.cpp
+++ cpu_scheduling/sjf-np.cpp
@@ -1,3 +1,7 @@
+// Name: Kartikey Tewari
+// Roll No.: 17MI551
+// Computer Science and Engineering (Dual Degree)
+// National Institute of Technology, Hamirpur
 #include <bits/stdc++.h>
 using namespace std;
 
--- cpu_scheduling/sjf-p.cpp
+++ cpu_scheduling/sjf-p.cpp
@@ -14,6 +14,7 @@ struct process
     int end_time;
     int waiting_time;
     int turn_around_time;
+    int rem_time;
 };
 
 vector<process> process_data;
@@ -25,7 +26,9 @@ void spawn_process(int n)
         process temp;
         temp.id=i;
         temp.arrival_time=(rand()%100)+1;;
+        temp.start_time=-1;
         temp.duration=(rand()%100)+1;
+        temp.rem_time=temp.duration;
         process_data.push_back(temp);
     }
 }
@@ -69,27 +72,53 @@ int main()
 
     spawn_process(n);
     sort(process_data.begin(),process_data.end(),compare);
-    
+
     int t=0;
     int avg_wt=0;
     int avg_tat=0;
+    int comp_process=0;
+    while (comp_process<=n-1)
+    {
+        vector<pair<int,int>> buffer;
+        for (int i=0;i<=n-1;i++)
+        {
+            if ((process_data[i].rem_time!=0)&&(process_data[i].arrival_time<=t))
+            {
+                buffer.push_back(make_pair(process_data[i].rem_time,i));
+            }
+        }
+
+        if (buffer.size()!=0)
+        {
+            sort(buffer.begin(),buffer.end());
+            int local_id=buffer.begin()->second;
+            process_data[local_id].rem_time--;
+            if (process_data[local_id].start_time==-1)
+            {
+                process_data[local_id].start_time=t;
+                process_data[local_id].waiting_time=process_data[local_id].start_time-process_data[local_id].arrival_time;
+                avg_wt+=process_data[local_id].waiting_time;
+            }
+            if (process_data[local_id].rem_time==0)
+            {
+                process_data[local_id].end_time=t;
+                process_data[local_id].turn_around_time=process_data[local_id].end_time-process_data[local_id].arrival_time;
+                avg_tat+=process_data[local_id].turn_around_time;
+                comp_process++;
+            }
+        }
+
+        t++;
+    }
+
     for (int i=0;i<=n-1;i++)
     {
-        t=max(t,process_data[i].arrival_time);
-        process_data[i].start_time=t;
-        t+=process_data[i].duration;
-        process_data[i].end_time=t;
-        process_data[i].waiting_time=process_data[i].start_time-process_data[i].arrival_time;
-        process_data[i].turn_around_time=process_data[i].end_time-process_data[i].arrival_time;
-
-        avg_wt+=process_data[i].waiting_time;
-        avg_tat+=process_data[i].turn_around_time;
         print_info(i);
     }
 
-    avg_tat/=n;
     avg_wt/=n;
-
+    avg_tat/=n;
+    
     cout << "Average waiting time= " << avg_wt << endl;
     cout << "Average turn around time= " << avg_tat << endl;
 
